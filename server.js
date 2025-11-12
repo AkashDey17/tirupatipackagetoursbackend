1f1da594fd8d99ed674df4fd1cf549b6943d0b68
Akash Dey <akashdeypersonal17@gmail.com>
2025-11-13 01:40:13 +0530

    improvement in booking seat functionality

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ require("dotenv").config();
 
 
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://www.tirupatipackagetours.com",
+      "https://tirupatipackagetours.com",
+      "http://localhost:3000"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 
